79da0a44a05f9bb91289b4bbbad12c8848b55033
Sahil Meo <contact.sahilmeo@gmail.com>
2025-06-22 12:36:48 +0500

    Install some pakages and create utils files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ dotenv.config();
 import express from 'express'
 import mongoose from 'mongoose'
 import connectDB from './db/index.js'
-
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 connectDB()
+app = express()
+app.use(cors({
+     origin: `${process.env.DB_ORIGIN}`,
+     credentials: true,
+}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({
+     extended: true,
+     limit: "16kb"
+}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export { app }
