8985573f7cbf1fcc7e92bb673eddcb4f4f7910be
Sahil Meo <contact.sahilmeo@gmail.com>
2025-08-16 23:27:55 +0500

    create login and logout user route

--- src/controlers/user.controler.js
+++ src/controlers/user.controler.js
@@ -22,7 +22,13 @@ const registerUser = asyncHandler(async (req, res) => {
      }
 
      const avatarLocalPath = req.files?.avatar[0].path;
-     const coverImageLocalPath = req.files?.coverImage[0].path;
+     // const coverImageLocalPath = req.files?.coverImage[0].path;
+     let coverImageLocalPath = ""
+
+     if (req.file || req.files?.coverImage || req.files?.coverImage[0]) {
+          coverImageLocalPath = req.files?.coverImage[0]?.path || "";
+     }
+
      // console.log({ coverImageLocalPath, avatarLocalPath });
 
      if (!avatarLocalPath) {
@@ -60,3 +66,42 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 export { registerUser }
+
+export const loginUser = async (req, res) => {
+     try {
+
+          const { email, password } = req.body;
+          if ([email, password].some((field) => field?.trim() === "")) {
+               throw new ApiError(400, "all fields are required");
+          }
+
+          const userExists = await User.findOne({ email });
+          if (!userExists) {
+               throw new ApiError(404, "User not found");
+          }
+
+          
+
+     } catch (error) {
+          console.log("error occure while login user:", error.message);
+          throw new ApiError(500, "Something went wrong while login User")
+     }
+}
+
+export const logoutUser = async (req, res) => {
+     try {
+          const { userId } = req.body;
+          if (!userId) {
+               throw new ApiError(400, "User ID is required");
+          }
+
+          // Perform logout logic here (e.g., invalidate token, etc.)
+
+          return res.status(200).json(
+               new ApiResponse(200, null, "User logged out successfully")
+          );
+     } catch (error) {
+          console.log("error occure while logout user:", error.message);
+          throw new ApiError(500, "Something went wrong while logout User")
+     }
+}
